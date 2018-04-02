2c51361f7ded66cc24737ccfb73b758316be6af5
Evan Aguilar <evan@eaguil.com>
2018-04-02 16:15:30 -0500

    venv set up with Django :: fresh

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/awstech02/Documents/programming/Maintenance_Tracker/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
