62bca950b440255a29e77cc5d5f3632fda975b11
Evan Aguilar <evan@eaguil.com>
2018-04-02 16:21:39 -0500

    Set settings up with local time CST and add root path; localhost allowed

--- myMainTracker/settings.py
+++ myMainTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pv7m4v_8xpa&t@yr8^emtz+y+pv=6tf9itu7n(5zl18sns12t@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
